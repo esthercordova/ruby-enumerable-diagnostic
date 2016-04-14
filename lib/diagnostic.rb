a5c575661764491a2cf7b864adf9ca98738c759c
Antony Donovan <antony.donovan@generalassemb.ly>
2016-04-14 14:22:10 -0400

    Update solution
    Fix linter errors

--- lib/diagnostic.rb
+++ lib/diagnostic.rb
@@ -1,48 +1,51 @@
-# Ruby enumerable diagnostic
-class Diagnostic
+# Ruby enumerable assessment
+class Assessment
   # Question 1
-  # Write ruby code to return the longest entry in the list supplied.
-  # Assume each entry in the list implements a length method that returns
-  # a Fixnum.
-  def longest(list)
-    # Your code goes here
+  # Write ruby code to return the longest entry in the
+  # enumerable supplied.  Assume each entry implements a length
+  # method that returns a Fixnum.
+  def longest(enum)
+    enum.max { |a, b| a.length <=> b.length }
   end
 
   # Question 2
   # Write ruby code to return the sum of entries in the
-  # list supplied (i.e. apply the `+` operator).
-  # Assume each entry implements a `+` method.
-  def sum(list)
-    # Your code goes here
+  # enumerable supplied (i.e. apply the `+` operator)
+  # Assume each entry implements a `+` method
+  def sum(enum)
+    enum.reduce(&:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
-  # in the list of numbers are odd
+  # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.any?(&:odd?)
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
-  # list of numbers are even
+  # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all?(&:even?)
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
-  # entries from the list of words capitalized (i.e. 'foo' becomes 'Foo').
+  # entries from the words enumerable capitalized.
   def transform(words)
-    # Your code goes here
+    words.map(&:capitalize)
   end
 
   # Question 6
   # Write ruby code to read each line from the file
   # named filename. Concatenate the lines together after
   # replacing the trailing newline character with a
-  # space and return the result.
+  # space.  Remove the trailing space and return the result.
   def read_file(filename)
-    # Your code goes here
+    File.open(filename).reduce('') { |a, e| a << e.chomp + ' ' }.strip
+    # File.open(filename).each_with_object('') do |line, memo|
+    #   memo << line.chomp + ' '
+    # end.strip
   end
 end
